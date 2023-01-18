7a22f75583bc530e808a414d6f886a9e2431755c
ugoingnowhereboy <ugnw20@gmail.com>
2023-01-18 09:05:38 +0300

    Title size fixes for small screens

--- app/Components/Loaders.jsx
+++ app/Components/Loaders.jsx
@@ -31,7 +31,7 @@ export const Cardloader = () => {
           <div className='flex flex-col items-center'>
           <span className="button-coloring h-16 sm:w-96 w-48">
             <span className="button-background"></span>
-            <span className="button-text bg-base-200 text-2xl"><div class="bg-slate-700 rounded-md h-10 w-4/6 animate-pulse"></div></span>
+            <span className="button-text bg-base-200 text-2xl"><div className="bg-slate-700 rounded-md h-10 w-4/6 animate-pulse"></div></span>
           </span>
         </div> 
         <div className="card lg:w-[900px] md:w-[700px] w-[100%] bg-base-300 shadow-xl mb-8 mt-8">
