c08e5606895ebcc77fbc1920214fc2720cf0bca6
UGoingNoWhereBoy <ugnw20@gmail.com>
2023-06-16 21:03:36 -0400

    Outdated

--- app/Components/Hero.jsx
+++ app/Components/Hero.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Accounts from './Accounts'
+import Link from 'next/link'
 const Hero = () => {
 
   return (
     <div className="hero mt-24 mb-20 bg-base-100">
       <div className="hero-content text-center">
-          <div className="max-w-md">
+        <div className="max-w-md">
+          <Link href={'https://eyad.vercel.app'} className='text-xl font-bold border p-2 rounded'>New Portfolio: eyad.vercel.app</Link>
           <div className='flex flex-col'>
               <Accounts />
           </div>
--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
   <div className="navbar bg-base-200 fixed top-0 z-50">
   <div className="navbar-start">
-  <Link href='/' className="btn btn-ghost normal-case sm:text-2xl text-xl">Eyad's Portfolio</Link>
+  <Link href='https://eyad.vercel.app' className="btn btn-ghost normal-case sm:text-2xl text-xl">Eyad's Portfolio(outdated)</Link>
 
   </div>
   <div className="navbar-center">
